83709505390d453756991c83d9cb372b55afbc09
doreenmuriithi <106369081+doreenmuriithi@users.noreply.github.com>
2022-07-01 11:10:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+simple calculator
+created by Doreen Njeri
+on July,2022
+c89 compiler
+MIT license
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tsimple calculator!\n");
+    return 0;
+}
